efc999cedc40fb99a2a8b1b9634a87055101e1e4
DiegoLibJu <70904579+DiegoLibJu@users.noreply.github.com>
2026-09-20 21:25:47 -0600

    Cambio Nombre de Imagen

--- NobelPrice.py
+++ NobelPrice.py
@@ -9,7 +9,7 @@ from sklearn.naive_bayes import MultinomialNB
 
 
 st.write(''' # Predicción de categoría de Premio Nobel ''')
-st.image("Nobel.png", caption="Su creador fue el inventor sueco Alfred Nobel mediante su testamento en 1895.")
+st.image("AlfredNobel.jpg", caption="Su creador fue el inventor sueco Alfred Nobel mediante su testamento en 1895.")
 
 st.header('Texto')
 
